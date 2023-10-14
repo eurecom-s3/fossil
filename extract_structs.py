4e286041796a3f67ceda8153894cfc4bbb09b8c0
J0Blank <128307956+J0Blank@users.noreply.github.com>
2023-10-14 17:01:42 +0200

    fix: fix type hinting

--- extract_structs.py
+++ extract_structs.py
@@ -703,7 +703,7 @@ def load_data_files(dataset_directory:str) -> dict[str, Any]:
     """
     Loads data files from the dataset directory.
     The returned dictionary has the following keys and types:
-        - pointers: dict[int, tuple[int, int]]
+        - pointers: dict[int, int]
         - virtual_to_offset: addrspaces.IMOffsets
         - bitmap: bitarray.bitarray
         - strings: dict[int, str]
