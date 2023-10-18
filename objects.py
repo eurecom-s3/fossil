5194e7f4c00d7de04e48d916493a5256a9f95dd5
J0Blank <128307956+J0Blank@users.noreply.github.com>
2023-10-18 10:40:46 +0200

    refactor: refactor of `MemoryObject`

--- objects.py
+++ objects.py
@@ -1,53 +1,72 @@
-from re import X
 import numpy as np
 from statistics import mode
 from collections import Counter, defaultdict
 from bisect import bisect_left, bisect_right
-from typing import List, Dict, Set, Tuple
 import ctypes
 from struct import unpack
 import time
 import ipaddress
 
+from addrspaces import IMOffsets
+from bitarray import bitarray
+
 class MemoryObject:
     @classmethod
-    def prepare(cls, dump_ptrs: List, ptr_size: int, v2o, btm, strs, xrefs, functions, dumpfile):
-        cls.ptrs = dump_ptrs
-        cls.ptrs_sorted = sorted(dump_ptrs.keys())
-        cls.ptrs_set = set(cls.ptrs_sorted)
+    def prepare(
+        cls, 
+        pointers: dict[int,int], 
+        pointer_size: int, 
+        virtual_to_offset: IMOffsets, 
+        bitmap: bitarray, 
+        strings: dict[int,str], 
+        external_references: set[int], 
+        functions: set[int], 
+        elf_dump_file: str
+        ) -> None:
+
+        # Define object pointers
+        cls.pointers = pointers
+        cls.sorted_pointers = sorted(pointers.keys())
+        cls.pointers_set = set(cls.sorted_pointers)
         
-        cls.autoptrs_sorted = sorted((k for k,v in cls.ptrs.items() if k == v))
-        cls.autoptrs_set = set(cls.autoptrs_sorted)
+        # Define object autopointers
+        cls.sorted_autopointers = sorted((pointer for pointer,pointed in cls.pointers.items() if pointer == pointed))
+        cls.autopointers_set = set(cls.sorted_autopointers)
 
-        cls.strs = strs
-        cls.strs_sorted = sorted(strs.keys())
-        cls.strs_set = set(strs.keys())
+        # Define object strings
+        cls.strings = strings
+        cls.sorted_strings = sorted(strings.keys())
+        cls.strings_set = set(strings.keys())
 
+        # Define obejct pointer size, virtual to offset and bitmap
+        cls.pointer_size = pointer_size
+        cls.virtual_to_offset = virtual_to_offset
+        cls.bitmap = bitmap
 
-        cls.ptr_size = ptr_size
-        cls.v2o = v2o
-        cls.btm = btm
-
-        if ptr_size == 4:
-            cls.convf = lambda self, x: ctypes.c_uint32(x).value
+        # Define typings and conversions based on pointer size
+        if pointer_size == 4:
+            cls.uint_conversion_function = lambda self, x: ctypes.c_uint32(x).value
             cls.dtype = np.uint32
         else:
-            cls.convf = lambda self, x: ctypes.c_uint64(x).value
+            cls.uint_conversion_function = lambda self, x: ctypes.c_uint64(x).value
             cls.dtype = np.uint64
 
-        cls.xrefs = xrefs
-        cls.xref_referenced = set()
+        # Define external references
+        cls.external_references = external_references
+        cls.referenced_external_references = set()
 
-        cls.dumpfile = open(dumpfile, "rb")
+        # Define input dump file and functions
+        cls.elf_dump_file = open(elf_dump_file, "rb")
         cls.functions = functions
 
     @classmethod
-    def ptrs_is_null(cls, ptr):
-        elf_off = cls.v2o[ptr]
-        if elf_off == -1 or cls.btm[elf_off:elf_off + cls.ptr_size].any():
+    def is_pointer_null(cls, pointer:int) -> bool:
+        elf_offset = cls.virtual_to_offset[pointer]
+        if elf_offset == -1:
             return False
-        else:
-            return True
+        if cls.bitmap[elf_offset : elf_offset + cls.pointer_size].any():
+            return False
+        return True
 
 class PointersGroup(MemoryObject):
     def __init__(self, ptrs_list, offsets=tuple()):
@@ -71,8 +90,8 @@ class PointersGroup(MemoryObject):
         
         self.struct_type = -1                   # Index describing the struct
 
-        xrefs = self.xrefs.intersection(self.ptrs_list)
-        self.xref_referenced.update(xrefs)
+        xrefs = self.external_references.intersection(self.ptrs_list)
+        self.referenced_external_references.update(xrefs)
         self.referenced = bool(xrefs)
         self.abs_timestamps = {}
         self.rel_timestamps = {}
@@ -88,16 +107,16 @@ class PointersGroup(MemoryObject):
         return len(self.ptrs_list)
 
     def get_u64(self, offset):
-        self.dumpfile.seek(offset, 0)
+        self.elf_dump_file.seek(offset, 0)
         try:
-            return unpack("<Q", self.dumpfile.read(8))[0]
+            return unpack("<Q", self.elf_dump_file.read(8))[0]
         except:
             return None
 
     def get_u32(self, offset):
-        self.dumpfile.seek(offset, 0)
+        self.elf_dump_file.seek(offset, 0)
         try:
-            return unpack("<I", self.dumpfile.read(4))[0]
+            return unpack("<I", self.elf_dump_file.read(4))[0]
         except:
             return None
 
@@ -108,7 +127,7 @@ class PointersGroup(MemoryObject):
 
         for offset in range(self.shape[0], self.shape[0] + self.shape[1]):
             for ptr in self.ptrs_list:
-                file_offset = self.v2o[ptr + offset]
+                file_offset = self.virtual_to_offset[ptr + offset]
                 if file_offset == -1:
                     continue
                 
@@ -141,7 +160,7 @@ class PointersGroup(MemoryObject):
             localhost_found = False
             candidates.clear()
             for ptr in self.ptrs_list:
-                file_offset = self.v2o[ptr + offset]
+                file_offset = self.virtual_to_offset[ptr + offset]
                 if file_offset == -1:
                     continue
                 
@@ -232,7 +251,7 @@ class PointersGroup(MemoryObject):
 
         self.valid_near_offsets = {x for x in set(self.near_ptrs.keys()).difference(self.autostructural_offsets).difference(self.structural_offsets) if self.shape[0] <= x < self.shape[0] + self.shape[1]}
         
-        self.list_child_offsets = {offset for offset in self.valid_near_offsets if 0 < len(self.near_ptrs[offset][0].intersection(self.autoptrs_set)) < len(self.near_ptrs[offset][0]) }
+        self.list_child_offsets = {offset for offset in self.valid_near_offsets if 0 < len(self.near_ptrs[offset][0].intersection(self.autopointers_set)) < len(self.near_ptrs[offset][0]) }
         self.list_child_offsets.difference_update(self.dests_offsets)
         self.list_child_offsets.difference_update(self.autostructural_offsets)
 
@@ -248,7 +267,7 @@ class PointersGroup(MemoryObject):
         """
         base_ptrs = sorted(self.ptrs_list)
         len_base_ptrs = len(base_ptrs)
-        len_dump_ptrs = len(self.ptrs)
+        len_dump_ptrs = len(self.pointers)
 
         visited_ptrs = defaultdict(list)
 
@@ -258,15 +277,15 @@ class PointersGroup(MemoryObject):
         min_idx = 0
         for base_ptr in base_ptrs:
             
-            min_idx = bisect_right(self.ptrs_sorted, base_ptr - mode_distance, lo=min_idx)
+            min_idx = bisect_right(self.sorted_pointers, base_ptr - mode_distance, lo=min_idx)
             
             for idx in range(min_idx, len_dump_ptrs):
-                diff = self.ptrs_sorted[idx] - base_ptr
+                diff = self.sorted_pointers[idx] - base_ptr
 
                 if diff >= mode_distance:
                     break
                 
-                visited_ptrs[diff].append(self.ptrs_sorted[idx])
+                visited_ptrs[diff].append(self.sorted_pointers[idx])
 
         # Add locations point to NULL
         nulls = defaultdict(int)
@@ -274,9 +293,9 @@ class PointersGroup(MemoryObject):
             for base_ptr in base_ptrs:
                 
                 ptr = base_ptr + offset
-                if ptr in self.ptrs_set or \
-                (elf_offset := self.v2o[ptr]) == -1 or \
-                self.btm[elf_offset:elf_offset+self.ptr_size].any():
+                if ptr in self.pointers_set or \
+                (elf_offset := self.virtual_to_offset[ptr]) == -1 or \
+                self.bitmap[elf_offset:elf_offset+self.pointer_size].any():
                     continue
                 
                 nulls[offset] += 1
@@ -312,16 +331,16 @@ class PointersGroup(MemoryObject):
             # pointers point to the starts of same type structures of the structure
             # under analysis there will be an common offset (at least in threshold%
             # of the cases)
-            dests = [self.ptrs[x] for x in ptrs_s if x not in self.autoptrs_set]
+            dests = [self.pointers[x] for x in ptrs_s if x not in self.autopointers_set]
             if not dests: # Ignore offsets containing only autopointers
                 continue
-            minimum_distances = [self.convf( np.min(base_ptrs_array - x) ) for x in dests] # Collect for each pointer the minimum distance between its destination and all the base pointers
+            minimum_distances = [self.uint_conversion_function( np.min(base_ptrs_array - x) ) for x in dests] # Collect for each pointer the minimum distance between its destination and all the base pointers
             if not minimum_distances:
                 continue
             common_distance = Counter(minimum_distances).most_common(1)[0]
             distance, count = common_distance
             if count < min(threshold * len(ptrs_s), len(ptrs_s) - 1) or \
-                distance >= max_offset or -distance > offset or not (-distance <= offset <= -distance + self.mode_distance - self.ptr_size): # Ignore too big distances
+                distance >= max_offset or -distance > offset or not (-distance <= offset <= -distance + self.mode_distance - self.pointer_size): # Ignore too big distances
                 continue
 
             offset_min_distance[offset] = distance
@@ -338,8 +357,8 @@ class PointersGroup(MemoryObject):
         # Check if te structure starting at possible_start contains all the
         # structural offsets
         possible_start = -possible_start
-        if possible_start > 0 or  not (possible_start  <= min(self.structural_offsets) <= max(self.structural_offsets) <= possible_start + self.mode_distance - self.ptr_size) or \
-           any([not (possible_start  <= x <= possible_start + self.mode_distance - self.ptr_size) for x in near_ptrs_same_type]):
+        if possible_start > 0 or  not (possible_start  <= min(self.structural_offsets) <= max(self.structural_offsets) <= possible_start + self.mode_distance - self.pointer_size) or \
+           any([not (possible_start  <= x <= possible_start + self.mode_distance - self.pointer_size) for x in near_ptrs_same_type]):
             return possible_start, []
         
         near_ptrs_same_type.sort()
@@ -351,7 +370,7 @@ class PointersGroup(MemoryObject):
         """
         valid_ptrs_per_window = []
         offsets = sorted([k for k,t in self.near_ptrs.items() \
-            if (abs(k-max(self.structural_offsets) - self.ptr_size) <= self.mode_distance and \
+            if (abs(k-max(self.structural_offsets) - self.pointer_size) <= self.mode_distance and \
                abs(k-min(self.structural_offsets)) <= self.mode_distance and \
                1 - t[1]/len(t[0]) > min(threshold, 1 - 1/len(t[0]))) or k in self.structural_offsets]) # Consider only offsets with at least threshold% of not null pointers
         ptrs_len = [len(self.near_ptrs[x][0]) for x in offsets]
@@ -361,7 +380,7 @@ class PointersGroup(MemoryObject):
         for idx in range(0, idx_final):
             total = 0
             offsets_counter = 0
-            limit = offsets[idx] + self.mode_distance - self.ptr_size
+            limit = offsets[idx] + self.mode_distance - self.pointer_size
             for idx2 in range(idx, idx_final):
                 if offsets[idx2] > limit:
                     break
@@ -385,7 +404,7 @@ class PointersGroup(MemoryObject):
         Find pointed and embedded strings
         """
         len_base_ptrs = len(self.ptrs_list)
-        len_strs = len(self.strs)
+        len_strs = len(self.strings)
 
         embded_strs = defaultdict(list)
         pointed_strs = defaultdict(list)
@@ -396,27 +415,27 @@ class PointersGroup(MemoryObject):
             for offset in self.valid_near_offsets:
                             
                 addr = elem + offset
-                if addr in self.autoptrs_set:
+                if addr in self.autopointers_set:
                     continue
 
                 try:
-                    pointed_addr = self.ptrs[addr]
-                    if pointed_addr in self.strs:
+                    pointed_addr = self.pointers[addr]
+                    if pointed_addr in self.strings:
                         pointed_strs[offset].append(pointed_addr)
                 except KeyError:
                     pass
             
             # Find embedded strings
             minimum_address = elem + self.shape[0]
-            min_idx = bisect_right(self.strs_sorted, minimum_address)
+            min_idx = bisect_right(self.sorted_strings, minimum_address)
         
             for idx in range(min_idx, len_strs):
-                diff = self.strs_sorted[idx] - elem
+                diff = self.sorted_strings[idx] - elem
 
                 if diff >= self.shape[0] + self.shape[1]:
                     break
                 
-                embded_strs[diff].append(self.strs_sorted[idx])
+                embded_strs[diff].append(self.sorted_strings[idx])
 
 
         real_threshold = min(len_base_ptrs - 1, threshold * len_base_ptrs)
@@ -437,21 +456,21 @@ class PointersGroup(MemoryObject):
         string = string.lower()
         for offset, s_addresses in self.embedded_strs.items():
             for s_address in s_addresses:
-                if string in (r := self.strs[s_address]):
+                if string in (r := self.strings[s_address]):
                     results[(offset, False)] = r
 
         for offset, s_addresses in self.pointed_strs.items():
             for s_address in s_addresses:
-                if string in (r := self.strs[s_address]):
+                if string in (r := self.strings[s_address]):
                     results[(offset, True)] = r
 
         return tuple(results)
 
     def extract_strings(self, offset, is_pointed):
         if is_pointed:
-            return [self.strs[x] for x in self.pointed_strs[offset]]
+            return [self.strings[x] for x in self.pointed_strs[offset]]
         else:
-            return [self.strs[x] for x in self.embedded_strs[offset]]
+            return [self.strings[x] for x in self.embedded_strs[offset]]
 
 class LinkedList(PointersGroup):
     def __init__(self, base_ptrs, offsets, is_ciclic):
@@ -464,22 +483,22 @@ class LinkedList(PointersGroup):
         """
         Return True if the list is terminated (NULL or autoptrs ending), False (data ending) otherwise
         """
-        if self.ptrs_list[-1] in self.autoptrs_set:
+        if self.ptrs_list[-1] in self.autopointers_set:
             return True
 
         try:
-            deref = self.ptrs[self.ptrs_list[-1]] + self.dests_offsets
+            deref = self.pointers[self.ptrs_list[-1]] + self.dests_offsets
             if deref  == self.ptrs_list[-1]:
                 return True
 
-            elf_off = self.v2o[deref]
-            if elf_off == -1 or self.btm[elf_off:elf_off + self.ptr_size].any():
+            elf_off = self.virtual_to_offset[deref]
+            if elf_off == -1 or self.bitmap[elf_off:elf_off + self.pointer_size].any():
                 return False
         except:
             pass
 
-        elf_off = self.v2o[self.ptrs_list[-1]]
-        if elf_off == -1 or self.btm[elf_off:elf_off + self.ptr_size].any():
+        elf_off = self.virtual_to_offset[self.ptrs_list[-1]]
+        if elf_off == -1 or self.bitmap[elf_off:elf_off + self.pointer_size].any():
             return False
         else:
             return True
@@ -539,8 +558,8 @@ class Tree(PointersGroup):
         tree_list = []
         for ptr in self.nodes:
             try:
-                if ptr and self.ptrs[ptr + offset] in self.pointed_strs[offset]:
-                    tree_list.append(self.ptrs[ptr + offset])
+                if ptr and self.pointers[ptr + offset] in self.pointed_strs[offset]:
+                    tree_list.append(self.pointers[ptr + offset])
                 else:
                     tree_list.append(None)
             except Exception as e:
@@ -552,13 +571,13 @@ class Tree(PointersGroup):
 class PtrsArray(MemoryObject):
     def __init__(self, ptrs_list):
         self.ptrs_list = ptrs_list       # Array of pointers
-        dest_ptrs = {self.ptrs[x] for x in ptrs_list}
-        self.referenced = bool(self.xrefs.intersection(self.ptrs_list))
+        dest_ptrs = {self.pointers[x] for x in ptrs_list}
+        self.referenced = bool(self.external_references.intersection(self.ptrs_list))
         self.structs = None
         self.referenced_fn = bool(self.functions.intersection(self.ptrs_list))
 
         # Find if all the pointers point directly to strings
-        if dest_ptrs.issubset(self.strs_set):
+        if dest_ptrs.issubset(self.strings_set):
             self.strs_array = list(dest_ptrs)
 
         else:
